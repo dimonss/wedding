da30130a7358ec203d4e23514177ffcb0f81d5cb
Chalysh Dmitrii <faimos69@gmail.com>
2025-04-14 23:05:14 +0600

    update layout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import useRejectGuest from "./hook/useRejectGuest";
 import useAdminAuth from "./hook/useAdminAuth";
 import Loader from "./component/loader/Loader";
 import GuestTable from "./component/admin/GuestTable";
+import mapImage from "./images/map.jpg";
 
 function App() {
     const [firstRender, setFirstRender] = useState(true);
@@ -18,6 +19,7 @@ function App() {
     const {guestData, guestLoading, guestError, fetchGuestData} = useGuestData(guestUUID);
     const {acceptCallback, acceptLoading, acceptError} = useAcceptGuest(guestUUID);
     const {rejectCallback, rejectLoading, rejectError} = useRejectGuest(guestUUID);
+
     useEffect(() => {
         if (!acceptLoading && !rejectLoading) {
             if (!firstRender) fetchGuestData();
@@ -49,33 +51,61 @@ function App() {
             <div className="invitation-container">
                 {guestData ? (
                     <>
-                        <h1>💍 Приглашение на свадьбу 💍</h1>
-                        <p className="welcome-text">{guestData?.fullName || 'Дорогие друзья и близкие!'}</p>
-                        <p>
-                            Мы, <span className="highlight">Дмитрий</span> и <span
-                            className="highlight">Екатерина</span>,
-                            с радостью приглашаем вас на наше свадебное торжество!
-                        </p>
-                        <p className="date">📅 Дата: 20 июля 2025 года</p>
-                        <p className="location">📍 Место: Ресторан "Белый сад", г. Москва</p>
-                        <p className="ending-text">Будем рады видеть вас в этот особенный день!</p>
-                        <button
-                            className="rsvp-button accept"
-                            onClick={acceptCallback}
-                        >
-                            {acceptLoading ? <Loader/> : '💌 Я буду!'}
-                        </button>
-                        <button
-                            className="rsvp-button reject"
-                            onClick={rejectCallback}
-                        >
-                            {rejectLoading ? <Loader/> : '😩 Сори не получается'}
-                        </button>
+                        <div className="header">
+                            <h5>{'дата \nи время'}</h5>
+                            <h1 className="title">СВАДЬБА<br/>И РЕГИСТРАЦИЯ</h1>
+                            <h5>{'где и как \nпроехать'}</h5>
+                        </div>
+                        <div className="names">
+                            <h2>Дмитрия & Екатерины</h2>
+                        </div>
+                        <div className="header-text">
+                            <div className="date-time">14-го июня 2025 года</div>
+                            <div className="guest-arrival">16:30—17:30 сбор гостей</div>
+                        </div>
+                        <div className="guest-name">
+                            {guestData?.fullName || '%USERNAME%'}
+                        </div>
+                        <div className="invitation-text">
+                            <p>Мы, Дмитрий и Екатерина,<br/>
+                               с радостью приглашаем вас<br/>
+                               на наше свадебное<br/>
+                               торжество!</p>
+                        </div>
+                        <p className="special-day">Будем рады видеть вас в этот особенный день!</p>
+                        <div className="rsvp-buttons">
+                            <button
+                                className="rsvp-button accept"
+                                onClick={acceptCallback}
+                            >
+                                {acceptLoading ? <Loader/> : '💌 Я приду!'}
+                            </button>
+                            <button
+                                className="rsvp-button reject"
+                                onClick={rejectCallback}
+                            >
+                                {rejectLoading ? <Loader/> : '😩 Не приду, сори'}
+                            </button>
+                        </div>
+                        <div className="venue-details">
+                            <h3>📅 Дата и время:</h3>
+                            <p>14-го июня 2025 года</p>
+                            <h3>📍 Место, как проехать:</h3>
+                            <p>«Кленовый лист» Гольф-клуб<br/>
+                               ул. Фрунзе, 1 с. Кара-Джыгач,<br/>
+                               Аламудунский район, Чуйская область</p>
+                        </div>
+                        <div className="map">
+                            <img src={mapImage} alt="map"/>
+                        </div>
+                        <div className="footer-text">
+                            Ждем с нетерпением 💝
+                        </div>
                     </>
                 ) : guestLoading ? (
                     <Loader/>
                 ) : (
-                    '😩пользователь не найден😩'
+                    <div className="error-message">😩 пользователь не найден 😩</div>
                 )}
             </div>
         </div>
