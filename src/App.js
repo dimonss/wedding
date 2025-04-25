0d53750eb6ffa1badab425e73e3ddce73f85ee19
Chalysh Dmitrii <faimos69@gmail.com>
2025-04-25 09:05:28 +0600

    update mobile layout && add logic

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import GuestTable from "./component/admin/GuestTable";
 
 function App() {
     const [firstRender, setFirstRender] = useState(true);
+    const [respStatus, setRespStatus] = useState(null);
     const {isAdmin, guestList, guestListError, guestListLoading} = useAdminAuth();
     useConfetti(isAdmin);
     let guestUUID = window.location.pathname.split('/').pop();
@@ -18,7 +19,9 @@ function App() {
     const {guestData, guestLoading, guestError, fetchGuestData} = useGuestData(guestUUID);
     const {acceptCallback, acceptLoading, acceptError} = useAcceptGuest(guestUUID);
     const {rejectCallback, rejectLoading, rejectError} = useRejectGuest(guestUUID);
-
+    useEffect(() => {
+        setRespStatus(guestData?.respStatus)
+    }, [guestData]);
     useEffect(() => {
         if (!acceptLoading && !rejectLoading) {
             if (!firstRender) fetchGuestData();
@@ -72,26 +75,43 @@ function App() {
                             {guestData?.fullName || '%USERNAME%'}
                         </div>
                         <div className="invitation-text">
-                            <p>Мы, Дмитрий и Екатерина,<br/>
-                                с радостью приглашаем Вас<br/>
-                                на наше свадебное<br/>
-                                торжество!</p>
+                            {respStatus === null &&
+                                <p>Мы, Дмитрий и Екатерина,<br/>
+                                    с радостью приглашаем Вас<br/>
+                                    на наше свадебное<br/>
+                                    торжество!</p>
+                            }
+                            {respStatus === 1 &&
+                                <p>Ждем с нетерпением 🤗</p>
+                            }
+                            {respStatus === 0 &&
+                                <p>Жаль что не получится!<br/>
+                                    Но если что то:</p>
+                            }
                         </div>
-                        <p className="special-day">Будем рады видеть Вас в этот особенный день!</p>
+                        {respStatus === null &&
+                            <p className="special-day">Будем рады видеть Вас в этот особенный день!</p>}
                         <div className="rsvp-buttons">
                             <div className="rsvp-buttons__container">
-                                <button
+                                {respStatus === null ? <>
+                                    <button
+                                        className="rsvp-button accept"
+                                        onClick={acceptCallback}
+                                    >
+                                        {acceptLoading ? <Loader/> : '💌 Я приду!'}
+                                    </button>
+                                    <button
+                                        className="rsvp-button reject"
+                                        onClick={rejectCallback}
+                                    >
+                                        {rejectLoading ? <Loader/> : '😩 Не приду, сори'}
+                                    </button>
+                                </> : <button
                                     className="rsvp-button accept"
-                                    onClick={acceptCallback}
+                                    onClick={() => setRespStatus(null)}
                                 >
-                                    {acceptLoading ? <Loader/> : '💌 Я приду!'}
-                                </button>
-                                <button
-                                    className="rsvp-button reject"
-                                    onClick={rejectCallback}
-                                >
-                                    {rejectLoading ? <Loader/> : '😩 Не приду, сори'}
-                                </button>
+                                    {`🧐 Я передумал${guestData?.gender === 'female' ? 'a' : ''}`}
+                                </button>}
                             </div>
                         </div>
                         <div className="venue-details" id="date">
@@ -106,21 +126,30 @@ function App() {
                             <iframe
                                 title="map"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3159.071531346306!2d74.70194022969577!3d42.80614583558391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb341e2128787%3A0x3ad083a24f096c54!2sBishkek%20Golf%20Club!5e0!3m2!1sen!2skg!4v1745081385779!5m2!1sen!2skg"
-                                width="420" height="300" style={{border: 0, borderRadius: '16px', maxWidth: "100%"}} allowFullScreen="" loading="lazy"
+                                width="420" height="300" style={{border: 0, borderRadius: '16px', maxWidth: "100%"}}
+                                allowFullScreen="" loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
-                        <div className="footer-text">
+                        {respStatus === null && <div className="footer-text">
                             Ждем с нетерпением 🤗
-                        </div>
+                        </div>}
+                        {respStatus !== null && <a
+                            href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Свадьба%20Дмитрия%20и%20Екатерины&dates=20250614T103000Z/20250614T160000Z&details=Сбор%20гостей%20в%2016:30.%20Место%20проведения:%20«Кленовый%20лист»%20Гольф-клуб&location=ул.%20Фрунзе,%201%20с.%20Кара-Джыгач,%20Аламудунский%20район,%20Чуйская%20область"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="calendar-button"
+                        >
+                            <span>📅 Добавить в календарь</span>
+                        </a>}
                     </>
-                    ) : guestLoading ? (
+                ) : guestLoading ? (
                     <Loader/>
-                    ) : (
+                ) : (
                     <div className="error-message">😩 пользователь не найден 😩</div>
-                    )}
+                )}
             </div>
         </div>
-);
+    );
 }
 
 export default App;
