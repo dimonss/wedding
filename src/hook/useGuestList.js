cd1225b484da014f7afe82c25a45ae8ce22b6429
Chalysh Dmitrii <faimos69@gmail.com>
2025-05-26 22:33:10 +0600

    fix API endpoints

--- src/hook/useGuestList.js
+++ src/hook/useGuestList.js
@@ -42,7 +42,7 @@ const useGuestList = (credentials) => {
     const createGuest = async (guestData) => {
         setLoading(true);
         try {
-            const response = await fetch(`${API_BASE_URL}/guests`, {
+            const response = await fetch(`${API_BASE_URL}/guest`, {
                 method: 'POST',
                 headers: getHeaders(),
                 body: JSON.stringify(guestData)
@@ -64,7 +64,7 @@ const useGuestList = (credentials) => {
     const updateGuest = async (guestId, guestData) => {
         setLoading(true);
         try {
-            const response = await fetch(`${API_BASE_URL}/guests/${guestId}`, {
+            const response = await fetch(`${API_BASE_URL}/guest/${guestId}`, {
                 method: 'PUT',
                 headers: getHeaders(),
                 body: JSON.stringify(guestData)
@@ -86,7 +86,7 @@ const useGuestList = (credentials) => {
     const deleteGuest = async (guestId) => {
         setLoading(true);
         try {
-            const response = await fetch(`${API_BASE_URL}/guests/${guestId}`, {
+            const response = await fetch(`${API_BASE_URL}/guest/${guestId}`, {
                 method: 'DELETE',
                 headers: getHeaders()
             });
